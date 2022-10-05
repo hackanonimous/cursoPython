83b4b746827a2b6a5d873cfb04ff197611024a31
hackanonimous <esi.segurity@gmail.com>
2022-10-05 12:11:02 -0500

    resumen de tipos de datos completado

---
+++ tiposdeDatos/tipodatos.py
@@ -0,0 +1,14 @@
+##este es un tipo de dato numerico
+9
+##estes es un tipo de dato numerico tipo float
+9.5
+##estes es un ti[po numerico grande
+9E
+##este es un tipo de dato string
+'este es estring'
+##tipo de dato booleano existe dos 
+
+##true
+True
+##false
+False
